526d0a009c5cf1802f54a061ebfdf20ad5541d97
Chris <cmsd2@cantab.net>
2015-01-07 22:16:18 +0000

    use R-Pi B+ gpio pins

--- source/main.s
+++ source/main.s
@@ -3,12 +3,12 @@
 _start:
     ldr r0, =0x20200000
     mov r1, #1
-    lsl r1, #18
-    str r1, [r0, #4]
+    lsl r1, #21
+    str r1, [r0, #16]
 
     mov r1, #1
-    lsl r1, #16
-    str r1, [r0, #40]
+    lsl r1, #15
+    str r1, [r0, #32]
 
     loop$:
     b loop$
